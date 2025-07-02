4c69d6f10f8ac68a0ad2580ae571f001a1b4311a
nihitha <nihithajumjuri@gmail.com>
2025-07-02 18:25:03 -0400

    backened

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,6 +2,12 @@ provider "aws" {
   region = var.aws_region
 }
 terraform {
+  backend "s3" {
+    bucket         = "nihi9-terraform-state-bucket"
+    key            = "finance/terraform.tfstate"
+    region         = var.aws_region
+
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
